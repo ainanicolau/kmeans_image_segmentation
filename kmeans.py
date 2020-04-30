1981429558dac5b4f8a8eba54cc0aa4a17ef42bd
ainanicolau <ainaa.nic@gmail.com>
2020-05-01 00:19:37 +0100

    clean code

--- kmeans.py
+++ kmeans.py
@@ -7,115 +7,133 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def parseArguments():
-        parser = argparse.ArgumentParser(
-                description="Implementation of k-means algorithm")
-        parser.add_argument(
-                "--k", type=int, default= 3,
-                help="number of clusters")
-        parser.add_argument(
-                "--numSamples", type=int, default= 20,
-                help="number of samples")
-        parser.add_argument(
-                "--amplitude", type=int, default= 5,
-                help="amplitude of the samples' position")
-        args = parser.parse_args()
-        return args
+def parse_arguments():
+    """
+    Parses command line options.
+    """
+    parser = argparse.ArgumentParser(description="Implementation of k-means "
+                                                 "algorithm")
+    parser.add_argument("--k", type=int, default=3, help="number of clusters")
+    parser.add_argument("--num_samples", type=int, default=20,
+                        help="number of samples")
+    parser.add_argument("--amplitude", type=int, default=5,
+                        help="amplitude of the samples' position")
+    args = parser.parse_args()
+    return args
 
 
-def generateCentroids(k, samples):
-        maxValues = np.max(samples, axis=0)
-        centroidsX = np.random.random(k)*maxValues[0]
-        centroidsY = np.random.random(k)*maxValues[1]
-        centroids = np.array([centroidsX,centroidsY]).transpose()
+def generate_centroids(k, samples):
+    """
+    Assigns a random index to each sample and create k centroids which position
+    is the mean of the samples with the corresponding index.
+    """
+    centroid_index = np.random.randint(k, size=len(samples))
 
-        plots = plotPoints(centroids, samples)
+    centroid_list = []
+    for centroid in range(k):
+        centroid_samples = samples[np.where(centroid_index == centroid)]
+        centroid_list.append(np.mean(centroid_samples, axis=0))
+    centroids = np.array(centroid_list)
 
-        return centroids, plots
+    plots = []
+    plot_points(centroids, samples, centroid_index, plots)
 
-
-def createClusters(centroids, samples, plots):
-        dist=[]
-        for centroid in centroids:
-                newDist = np.linalg.norm(centroid-samples, axis=1)
-                dist.append(newDist)
-
-        centroidIndex = np.argmin(dist, axis=0)
-
-        plots = plotPoints(centroids, samples, centroidIndex, plots)
-
-        return centroidIndex, plots
+    return centroids, plots
 
 
-def updateCentroid(centroids, samples, centroidIndex, plots):
-        stop = True
-        for centroid in range(len(centroids)):
-                newPosition = np.mean(
-                    samples[np.where(centroidIndex==centroid)],axis=0)
-                if not (np.sum(newPosition - centroids[centroid])==0):
-                        stop = False
-                centroids[centroid] = newPosition
+def create_clusters(centroids, samples, plots):
+    """
+    Computes the distance between each sample and centroid to assign the samples
+    to the closest centroid index.
+    """
+    dist = []
+    for centroid in centroids:
+        new_dist = np.linalg.norm(centroid - samples, axis=1)
+        dist.append(new_dist)
 
-        plots = plotPoints(centroids, samples, centroidIndex)
+    centroid_index = np.argmin(dist, axis=0)
 
-        return centroids, stop, plots
+    plot_points(centroids, samples, centroid_index, plots)
 
-
-def plotPoints(centroids, samples, centroidIndex=np.array([]), plots=[]):
-        plotPoints = []
-        colors = cm.rainbow(np.linspace(0, 1, len(centroids)))
-        for index in range(len(centroids)):
-                plotPoints.append(plt.plot(
-                    centroids[index,0],
-                    centroids[index,1], 
-                    color=colors[index], 
-                    marker='o')[0])
-        
-        if centroidIndex.any():
-                for index in range(len(samples)):
-                        plotPoints.append(plt.plot(
-                            samples[index,0],
-                            samples[index,1], 
-                            color=colors[centroidIndex[index]], 
-                            marker='.')[0])
-        else:
-                plotPoints.append(plt.plot(samples[:,0],samples[:,1],'k.')[0])
-
-        plots.append(plotPoints)
-
-        return plots
+    return centroid_index, plots
 
 
-def saveAnimation(fig, plots):
-        Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-        ani = animation.ArtistAnimation(
-            fig, plots, interval=50, repeat_delay=3000, blit=True)
-        ani.save('kmeans.mp4', writer=writer)
+def update_centroid(centroids, samples, centroid_index, plots):
+    """
+    Computes the new position for the centroids with the mean of the samples
+    that belong them. If none of the centroids is changing the position the
+    function returns the value to stop the algorithm.
+    """
+    stop = True
+    for centroid in range(len(centroids)):
+        new_position = np.mean(samples[np.where(centroid_index == centroid)],
+                               axis=0)
+        if not (np.sum(new_position - centroids[centroid]) == 0):
+            stop = False
+        centroids[centroid] = new_position
+
+    plot_points(centroids, samples, centroid_index, plots)
+
+    return centroids, stop, plots
 
 
-def main():       
-        args = parseArguments()
+def plot_points(centroids, samples, centroid_index, plots):
+    """
+    Plots the centroids and samples per iteration.
+    """
+    plot_points = []
+    colors = cm.rainbow(np.linspace(0, 1, len(centroids)))
+    for index in range(len(centroids)):
+        plot_points.append(plt.plot(centroids[index, 0], centroids[index, 1],
+                                    color=colors[index], marker='o')[0])
 
-        samples = np.random.random([args.numSamples,2])*args.amplitude
+    if centroid_index.any():
+        for index in range(len(samples)):
+            plot_points.append(plt.plot(samples[index, 0], samples[index, 1],
+                                        color=colors[centroid_index[index]],
+                                        marker='.')[0])
+    else:
+        plot_points.append(plt.plot(samples[:, 0], samples[:, 1], 'k.')[0])
 
-        fig = plt.figure()
-        plt.axis([-1, np.max(samples), -1, (np.max(samples))])
+    plots.append(plot_points)
 
-        # Initialization step
-        centroids, plots = generateCentroids(args.k, samples)
-        
-        stop = False
-        while (not stop):
-                # Assignment step
-                centroidIndex, plots = createClusters(centroids, samples, plots)
+    return plots
 
-                # Update step
-                centroids, stop, plots = updateCentroid(
-                    centroids, samples, centroidIndex, plots)
 
-        saveAnimation(fig, plots)
+def save_animation(fig, plots):
+    """
+    Creates an animated gif for the different iterations of the algorithm.
+    """
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=15, metadata=dict(artist='Aina'), bitrate=1800)
+    ani = animation.ArtistAnimation(
+        fig, plots, interval=50, repeat_delay=3000, blit=True)
+    ani.save('kmeans.mp4', writer=writer)
+
+
+def main():
+    args = parse_arguments()
+
+    # Create an array of samples to apply kmeans to
+    samples = np.random.random([args.num_samples, 2])*args.amplitude
+
+    fig = plt.figure()
+    plt.axis([-1, np.max(samples), -1, (np.max(samples))])
+
+    # Initialization step
+    centroids, plots = generate_centroids(args.k, samples)
+
+    stop = False
+    while (not stop):
+        # Assignment step
+        centroid_index, plots = create_clusters(centroids, samples, plots)
+
+        # Update step
+        centroids, stop, plots = update_centroid(
+            centroids, samples, centroid_index, plots)
+
+    save_animation(fig, plots)
 
 
 if __name__ == '__main__':
-        main()
+    main()
